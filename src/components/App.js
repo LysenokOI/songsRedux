57df8df8dd1fb0942a21827a5182fbc1abe13f5b
Oleg Lysenok <msk.lysenok@gmail.com>
2019-11-12 13:03:02 +0300

    added margin top

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,15 @@ import SongDetail from "./SongDetail";
 
 let App = () => {
   return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <SongList />
-        </div>
-        <div className="column eight wide">
-          <SongDetail />
+    <div className="ui container" style={{ marginTop: "3vh" }}>
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
         </div>
       </div>
     </div>
